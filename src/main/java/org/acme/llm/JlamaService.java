2e3ed360c36662f99dae55fa2bd832e244909299
RaedChami <raed.chami@laposte.net>
2025-10-26 13:23:51 +0000

    Adaptation à la maj du projet

--- src/main/java/org/acme/llm/JlamaService.java
+++ src/main/java/org/acme/llm/JlamaService.java
@@ -1,10 +1,9 @@
 package org.acme.llm;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import dev.langchain4j.model.chat.ChatModel;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.acme.model.Exercise;
@@ -22,19 +21,20 @@ public class JlamaService {
         Objects.requireNonNull(prompt);
         return model.chat(prompt);
     }
-    public Exercise generateExercise(String prompt) throws IOException {
+    public Exercise generateExercise(String prompt) {
         Objects.requireNonNull(prompt);
-        var detailedPrompt = model.chat(
-                "Pour le prompt suivant:" + prompt + "Génèrez un exercice au format JSON respectant les champs suivant:" +
-                        "description: énoncé détaillé de l'exercice avec des exemples d'utilisation" +
-                        "level: Un niveau académique recommandé pour l'exercice (L1,L2,L3,M1 ou M2) " +
-                        "concepts: Liste des concepts pertinents avec l'exercice" +
-                        "body: Signature de la fonction / Squelette de code" +
-                        "tests: Tests unitaires JUnit pour l'exercice" +
-                        "solution: Solution exemple pour l'exercice");
-        try (JsonParser parser = reader.createParser(detailedPrompt)) {
-            return parser.readValueAs(new TypeReference<Exercise>() {});
-        }
+        var answer = model.chat("<|begin_of_text|>" +
+                "<|start_header_id|>system<|end_header_id|>" +
+                "Vous êtes un expert en création d'énoncé d'exercices de programmation" +
+                "<|eot_id|>" +
+                "<|start_header_id|>user<|end_header_id|>" +
+                "Votre réponse ne doit pas contenir de code. Retournez un énoncé détaillé décrivant la simple description suivante"
+                        + prompt +
+                "<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
+                );
+        var generatedExercise = new Exercise();
+        generatedExercise.description = answer;
+        return generatedExercise;
     }
 
 }
