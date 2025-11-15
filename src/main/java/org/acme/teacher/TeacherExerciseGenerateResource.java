9150ba644faf119a0de1d28fc2223ec5b50bf5af
unknown <raed.chami@laposte.net>
2025-11-15 18:02:38 +0100

    Création d'un Enum pour la difficulté et Encapsulation renforcée pour la ressource exercice

--- src/main/java/org/acme/teacher/TeacherExerciseGenerateResource.java
+++ src/main/java/org/acme/teacher/TeacherExerciseGenerateResource.java
@@ -32,7 +32,7 @@ public class TeacherExerciseGenerateResource {
         Objects.requireNonNull(exercise);
         Exercise finalExercise = null;
         while (finalExercise == null) {
-            finalExercise = compile(exercise.description);
+            finalExercise = compile(exercise.getDescription());
         }
         return finalExercise;
     }
@@ -42,7 +42,7 @@ public class TeacherExerciseGenerateResource {
     @Path("/save")
     public Exercise save(Exercise exercise) {
         Objects.requireNonNull(exercise);
-        return (exercise.id != null) ? em.merge(exercise) : returnPersist(exercise);
+        return (exercise.getId() != null) ? em.merge(exercise) : returnPersist(exercise);
     }
 
     private Exercise returnPersist(Exercise exercise) {
@@ -56,9 +56,9 @@ public class TeacherExerciseGenerateResource {
         try {
             var generated = jlamaService.generateExercise(description);
             exerciseCompiler.createTemporaryDirectory();
-            var testPath = exerciseCompiler.createTemporaryFiles(generated.unitTests);
-            var solutionPath = exerciseCompiler.createTemporaryFiles(generated.solution);
-            if (!exerciseCompiler.compileCode(generated.unitTests, testPath) || !exerciseCompiler.compileCode(generated.solution, solutionPath)) {
+            var testPath = exerciseCompiler.createTemporaryFiles(generated.getUnitTests());
+            var solutionPath = exerciseCompiler.createTemporaryFiles(generated.getSolution());
+            if (!exerciseCompiler.compileCode(generated.getUnitTests(), testPath) || !exerciseCompiler.compileCode(generated.getSolution(), solutionPath)) {
                 System.out.println("Compilation failed, Exercise regeneration");
                 return null;
             }
