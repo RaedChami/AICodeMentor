deca92a59245e7555e3a01b06eee002efec35b04
RAED CHAMI <raed.chami@nomachine.univ-eiffel.fr>
2025-12-19 19:54:29 +0100

    Correction de warnings à la compilation

--- src/main/java/fr/uge/exercise/exception/ExerciseGenerationException.java
+++ src/main/java/fr/uge/exercise/exception/ExerciseGenerationException.java
@@ -1,6 +1,10 @@
 package fr.uge.exercise.exception;
 
+import java.io.Serial;
+
 public class ExerciseGenerationException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
     public ExerciseGenerationException(String message, Throwable cause) {
         super(message, cause);
     }
